c7a58528734c48c3c5b2125c85bb419b60f7f1c3
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-02-17 17:43:47 +0530

    Improved login screen

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -6,23 +6,61 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), labelText: "Enter Email Address"),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextField(
-            keyboardType: TextInputType.visiblePassword,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), labelText: "Enter Password"),
-          ),
-        ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage('assets/images/icon.png'),
+              height: 200,
+              width: double.infinity,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter Email Address"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Enter Password"),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black, 
+                foregroundColor: Colors.white, 
+                padding: EdgeInsets.symmetric(
+                    horizontal: 16, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                elevation: 5, 
+              ),
+              onPressed: () {
+                // Handle login action
+              },
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 18, 
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.2,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
