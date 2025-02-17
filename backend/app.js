2f4e8d53906b2f3e0953f21b001ad2237b30f633
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-02-17 18:11:57 +0530

    Added backend

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+const app = express()
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT,() =>{
+    console.log(`Server listening on port ${process.env.PORT}`)
+})
