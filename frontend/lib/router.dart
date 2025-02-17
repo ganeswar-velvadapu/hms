28327f3fd4894f57ccde0387d3818732e6fbe654
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-02-17 17:43:15 +0530

    Configured go router

--- frontend/lib/router.dart
+++ frontend/lib/router.dart
@@ -1,1 +1,20 @@
+import 'package:frontend/screens/home_screen.dart';
+import 'package:frontend/screens/login_screen.dart';
 import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  final GoRouter router = GoRouter(
+    initialLocation: '/login',
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => HomeScreen(), 
+      ),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => LoginScreen(), 
+      ),
+    ],
+  );
+}
+
